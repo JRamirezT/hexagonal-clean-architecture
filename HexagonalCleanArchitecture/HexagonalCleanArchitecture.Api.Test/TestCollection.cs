b434c62bfeb06cd67e67d336f367567ce823588e
Jhon Alexis Ramirez <jramirezt12@gmail.com>
2023-11-22 15:34:29 -0500

    Se carga avance de la creación de las pruebas

---
+++ HexagonalCleanArchitecture/HexagonalCleanArchitecture.Api.Test/TestCollection.cs
@@ -0,0 +1,10 @@
+﻿namespace HexagonalCleanArchitecture.Api.Test;
+
+/// <summary>
+/// Esta clase no tiene código y nunca se crea.Su propósito es simplemente
+//  ser el lugar para aplicar[CollectionDefinition] y todos los
+//  interfaces ICollectionFixture<>.
+/// </summary>
+
+[CollectionDefinition("ApiCollection collection")]
+public class ApiCollection : ICollectionFixture<ApiTestApp> { }
